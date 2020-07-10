4d516679afdf4e8985a20efc70512225b8dab20e
Tomás Carvalho <tomas.jpeg@gmail.com>
2020-07-10 10:29:05 -0300

    creating next appointment

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
@@ -28,6 +37,34 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 10</span>
+            <span>Sexxxta-feira</span>
+          </p>
+        </Schedule>
+
+        <NextAppointment>
+          <strong>Atendimento a seguir</strong>
+          <div>
+            <img
+              src="https://avatars3.githubusercontent.com/u/25818342?s=460&u=4b80f73a0f5f0cf6e3333526ba47452671ca0639&v=4"
+              alt="user"
+            />
+
+            <strong>Tomás Carvalho</strong>
+            <span>
+              <FiClock />
+              09:00
+            </span>
+          </div>
+        </NextAppointment>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
